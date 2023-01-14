fe87c03bba2364a0377568b77122e05455fc1f5f
elchibek1 <elchi.work00@gmail.com>
2023-01-14 21:20:38 +0600

    create 2 directory. in core directory create 3 classes for Requesting, Routing and Applicating, public directory has index.php file which all actions are performed

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+use app\core\Application;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Application();
+
+$app->router->get('/', function ()
+{
+    return "Hello world";
+});
+
+$app->router->get('/contact', function (){
+    return "Contact";
+});
+
+$app->run();

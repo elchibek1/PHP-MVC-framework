f412c3792fdbe969915986141dda57d82ef929e2
elchibek1 <elchi.work00@gmail.com>
2023-01-15 01:44:45 +0600

    add Response class, create models and runtime directories, change Application and Router classes

--- core/Application.php
+++ core/Application.php
@@ -3,21 +3,25 @@ namespace app\core;
 
 class Application
 {
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
-
+    public Response $response;
+    public static Application $app;
     /**
      * Application constructor.
      */
-    public function __construct()
+    public function __construct($rootPath)
     {
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
         $this->request = new Request();
+        $this->response = new Response();
         $this->router = new Router($this->request);
-
     }
 
     public function run()
     {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,15 +4,11 @@ use app\core\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Application();
 
-$app->router->get('/', function ()
-{
-    return "Hello world";
-});
+$app = new Application(dirname(__DIR__));
 
-$app->router->get('/contact', function (){
-    return "Contact";
-});
+$app->router->get('/', 'home');
+
+$app->router->get('/contact', 'contact');
 
 $app->run();
